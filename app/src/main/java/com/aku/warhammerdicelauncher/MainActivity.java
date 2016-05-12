dce3ae3edec8682ccbf26955859a6f74aaa588a8
CNicolas <clement92nicolas@gmail.com>
2016-05-12 10:47:30 +0200

    Added splash screen. App now support vertical screen

--- app/src/main/java/com/aku/warhammerdicelauncher/MainActivity.java
+++ app/src/main/java/com/aku/warhammerdicelauncher/MainActivity.java
@@ -3,7 +3,6 @@ package com.aku.warhammerdicelauncher;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.content.pm.ActivityInfo;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -41,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
     }
 
