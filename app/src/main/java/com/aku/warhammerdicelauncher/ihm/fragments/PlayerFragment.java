d8412b50b5e5d4e5bb4b0a5c1745714e7635220d
Clement NICOLAS <clement92nicolas@gmail.com>
2016-10-13 12:23:11 +0200

    Add icons on tabs in PlayerFragment

--- app/src/main/java/com/aku/warhammerdicelauncher/ihm/fragments/PlayerFragment.java
+++ app/src/main/java/com/aku/warhammerdicelauncher/ihm/fragments/PlayerFragment.java
@@ -2,12 +2,13 @@ package com.aku.warhammerdicelauncher.ihm.fragments;
 
 
 import android.app.Fragment;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTabHost;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
 import com.aku.warhammerdicelauncher.R;
 import com.aku.warhammerdicelauncher.ihm.activities.MainActivity;
@@ -16,6 +17,10 @@ import com.aku.warhammerdicelauncher.ihm.activities.MainActivity;
  * A simple {@link Fragment} subclass.
  */
 public class PlayerFragment extends Fragment {
+
+    final static String CHARACTERISTICS_FRAGMENT = "characteristicsFragment";
+    final static String SKILLS_FRAGMENT = "skillsFragment";
+    final static String INVENTORY_FRAGMENT = "inventoryFragment";
 
     public PlayerFragment() {
         // Required empty public constructor
@@ -30,16 +35,44 @@ public class PlayerFragment extends Fragment {
 
         tabHost.setup(context, context.getSupportFragmentManager(), android.R.id.tabcontent);
 
-        TabHost.TabSpec tab1 = tabHost.newTabSpec("characteristicsFragment").setIndicator(getString(R.string.page_characteristics));
-        TabHost.TabSpec tab2 = tabHost.newTabSpec("skillsFragment").setIndicator(getString(R.string.page_skills));
-        TabHost.TabSpec tab3 = tabHost.newTabSpec("inventoryFragment").setIndicator(getString(R.string.page_inventory));
-
-        tabHost.addTab(tab1, CharacteristicsFragment.class, null);
-        tabHost.addTab(tab2, SkillsFragment.class, null);
-        tabHost.addTab(tab3, InventoryFragment.class, null);
+        addCharacteristicsFragmentTab(context, tabHost);
+        addSkillsFragmentTab(context, tabHost);
+        addInventoryFragmentTab(context, tabHost);
 
         tabHost.bringChildToFront(rootView);
 
         return rootView;
+    }
+
+    private void addCharacteristicsFragmentTab(MainActivity context, FragmentTabHost tabHost) {
+        String indicatorTitle = getString(R.string.page_characteristics);
+        Drawable indicatorIcon = getIconByIdByAndroidVersion(context, R.drawable.ic_characteristics_black);
+
+        TabSpec characteristicsTab = tabHost.newTabSpec(CHARACTERISTICS_FRAGMENT).setIndicator("", indicatorIcon);
+        tabHost.addTab(characteristicsTab, CharacteristicsFragment.class, null);
+    }
+
+    private void addSkillsFragmentTab(MainActivity context, FragmentTabHost tabHost) {
+        String indicatorTitle = getString(R.string.page_skills);
+        Drawable indicatorIcon = getIconByIdByAndroidVersion(context, R.drawable.ic_skills_black);
+
+        TabSpec skillsTab = tabHost.newTabSpec(SKILLS_FRAGMENT).setIndicator("", indicatorIcon);
+        tabHost.addTab(skillsTab, SkillsFragment.class, null);
+    }
+
+    private void addInventoryFragmentTab(MainActivity context, FragmentTabHost tabHost) {
+        String indicatorTitle = getString(R.string.page_inventory);
+        Drawable indicatorIcon = getIconByIdByAndroidVersion(context, R.drawable.ic_rucksack_black);
+
+        TabSpec inventoryTab = tabHost.newTabSpec(INVENTORY_FRAGMENT).setIndicator("", indicatorIcon);
+        tabHost.addTab(inventoryTab, InventoryFragment.class, null);
+    }
+
+    private Drawable getIconByIdByAndroidVersion(MainActivity context, int iconId) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            return context.getDrawable(iconId);
+        } else {
+            return getResources().getDrawable(iconId);
+        }
     }
 }
