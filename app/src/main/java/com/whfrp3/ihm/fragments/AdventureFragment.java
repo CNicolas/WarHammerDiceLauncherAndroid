d0d7816011e52a76d4384fcbfb068134c5acea7d
Clement NICOLAS <clement.nicolas@younited-credit.fr>
2016-11-10 11:28:04 +0100

    Simplify CharacteristicsFragment, remove elements from AdventureFragment

--- app/src/main/java/com/whfrp3/ihm/fragments/AdventureFragment.java
+++ app/src/main/java/com/whfrp3/ihm/fragments/AdventureFragment.java
@@ -1,5 +1,6 @@
 package com.whfrp3.ihm.fragments;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -10,9 +11,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.whfrp3.R;
+import com.whfrp3.databinding.FragmentAdventureBinding;
 import com.whfrp3.ihm.listeners.PlayerEditTextWatcher;
 import com.whfrp3.ihm.listeners.StanceChangeListener;
+import com.whfrp3.tools.BindingContext;
 import com.whfrp3.tools.PlayerContext;
+import com.whfrp3.tools.constants.IPlayerConstants;
 import com.whfrp3.tools.enums.PlayerInformation;
 import com.whfrp3.tools.helpers.OnPlayerUpdateListener;
 
@@ -23,6 +27,7 @@ import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar;
  */
 public class AdventureFragment extends Fragment implements OnPlayerUpdateListener {
     private DiscreteSeekBar mPlayerStance;
+    private BindingContext mBindingContext;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,18 +37,29 @@ public class AdventureFragment extends Fragment implements OnPlayerUpdateListene
         View mRootView = inflater.inflate(R.layout.fragment_adventure, container, false);
         mPlayerStance = (DiscreteSeekBar) mRootView.findViewById(R.id.player_stance);
         EditText playerWoundsView = (EditText) mRootView.findViewById(R.id.player_wounds);
-        EditText playerCorruptionView = (EditText) mRootView.findViewById(R.id.player_corruption);
-        EditText playerExperienceView = (EditText) mRootView.findViewById(R.id.player_experience);
         TextView currentStanceTextView = (TextView) mRootView.findViewById(R.id.currentStance);
 
         onPlayerUpdate();
 
         mPlayerStance.setOnProgressChangeListener(new StanceChangeListener(getActivity(), currentStanceTextView));
         playerWoundsView.addTextChangedListener(new PlayerEditTextWatcher(PlayerInformation.WOUNDS));
-        playerExperienceView.addTextChangedListener(new PlayerEditTextWatcher(PlayerInformation.EXPERIENCE));
-        playerCorruptionView.addTextChangedListener(new PlayerEditTextWatcher(PlayerInformation.CORRUPTION));
 
         PlayerContext.registerPlayerUpdateListener(this);
+
+
+        //region BINDING
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            mBindingContext = (BindingContext) bundle.getSerializable(IPlayerConstants.BINDING_CONTEXT_KEY);
+        } else {
+            mBindingContext = new BindingContext(false);
+        }
+
+        FragmentAdventureBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_adventure, container, false);
+        binding.setPlayer(PlayerContext.getPlayerInstance());
+//        binding.setBindingContext(mBindingContext);
+        //endregion
+
 
         long difference = System.currentTimeMillis() - startTime;
         Log.d("AdventureFragment", String.format("%d = %d", startTime, difference));
