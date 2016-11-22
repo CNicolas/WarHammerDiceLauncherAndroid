f07d582be71e404c028ab87d131f93b26d2610c5
Clement NICOLAS <clement.nicolas@younited-credit.fr>
2016-11-22 16:12:22 +0100

    Continue style in dice numberpickers

--- app/src/main/java/com/whfrp3/ihm/components/NumberPickerMinMax.java
+++ app/src/main/java/com/whfrp3/ihm/components/NumberPickerMinMax.java
@@ -1,15 +1,15 @@
 package com.whfrp3.ihm.components;
 
-import android.annotation.TargetApi;
 import android.content.Context;
-import android.os.Build;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.widget.NumberPicker;
+
+import com.whfrp3.R;
 
 /**
  * Extension of the standard NumberPicker, providing the xml attributes minValue and maxValue.
  */
-@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class NumberPickerMinMax extends NumberPicker {
 
     //region Constructors matching super
@@ -19,18 +19,19 @@ public class NumberPickerMinMax extends NumberPicker {
 
     public NumberPickerMinMax(Context context, AttributeSet attrs) {
         super(context, attrs);
-        processAttributeSet(attrs);
+        processAttributeSet(context, attrs);
     }
 
     public NumberPickerMinMax(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        processAttributeSet(attrs);
+        processAttributeSet(context, attrs);
     }
     //endregion
 
-    private void processAttributeSet(AttributeSet attrs) {
-        //This method reads the parameters given in the xml file and sets the properties according to it
-        this.setMinValue(attrs.getAttributeIntValue(null, "minValue", 0));
-        this.setMaxValue(attrs.getAttributeIntValue(null, "maxValue", 0));
+    private void processAttributeSet(Context context, AttributeSet attrs) {
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.NumberPickerMinMax, 0, 0);
+
+        setMinValue(a.getInteger(R.styleable.NumberPickerMinMax_minValue, getContext().getResources().getInteger(R.integer.zero)));
+        setMaxValue(a.getInteger(R.styleable.NumberPickerMinMax_maxValue, getContext().getResources().getInteger(R.integer.max_dice_number_picker)));
     }
 }
