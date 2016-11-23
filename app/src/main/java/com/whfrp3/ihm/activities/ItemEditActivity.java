2703d5594bca847a29e1bec9be2aa786633a9b91
Clement NICOLAS <clement.nicolas@younited-credit.fr>
2016-11-23 16:37:21 +0100

    Style headers in ItemEditActivity

--- app/src/main/java/com/whfrp3/ihm/activities/ItemEditActivity.java
+++ app/src/main/java/com/whfrp3/ihm/activities/ItemEditActivity.java
@@ -79,7 +79,6 @@ public class ItemEditActivity extends AppCompatActivity {
         ActivityItemEditBinding binding = DataBindingUtil.inflate(getLayoutInflater(), R.layout.activity_item_edit, null, false);
         binding.setItem(itemEdit);
         binding.setViewModel(this);
-        binding.itemEditCommonFields.setItem(itemEdit);
         binding.itemEditWeaponFields.setItem(itemEdit);
         binding.itemEditArmorFields.setItem(itemEdit);
         binding.itemEditUsableFields.setItem(itemEdit);
